aa53b21f04c02b2152e7de08d54260020205b665
vinicios.santos <viniciossantos@useall.com.br>
2023-09-29 16:52:32 -0300

    fix

--- src/components/base/Welcome.tsx
+++ src/components/base/Welcome.tsx
@@ -1,7 +1,7 @@
 const Welcome = () => {
   return ( 
     <div>
-      <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-3xl lg:text-3xl" style={{margin: "0px 0px 10px 20px"}}>
+      <h1 className="mb-4 text-3xl font-extrabold text-white-900 dark:text-white md:text-3xl lg:text-3xl" style={{margin: "0px 0px 10px 20px"}}>
           Bem vindo ao
         <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#fec80a] to-[#d4a90b]" style={{margin: "0px 10px 0px 15px"}}>
           Yellow Software
